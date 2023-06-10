a0e263fa7924742910063dc154e20ced980742b5
Revenko Mykola <h_i@ukr.net>
2023-06-10 14:14:10 +0300

    add var

---
+++ Terraform/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.52.0"
+    }
+  }
+}
+
+provider "google" {
+  # Config options
+  project = var.GOOGLE_PROJECT
+  region  = var.GOOGLE_REGION
+}
+
+
+
+
+module "gke_cluster" {
+  source         = "github.com/nirev23/tf-google-gke-cluster"
+  GOOGLE_REGION  = var.GOOGLE_REGION
+  GOOGLE_PROJECT = var.GOOGLE_PROJECT
+  GKE_NUM_NODES  = 2
+}
