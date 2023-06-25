a27f96b5ddb4ee672b16865269c4eb0b6403312f
nirev23 <nick.revenko@gmail.com>
2023-06-25 12:58:16 +0000

    flux_begin

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -22,3 +22,27 @@ module "gke_cluster" {
   GOOGLE_PROJECT = var.GOOGLE_PROJECT
   GKE_NUM_NODES  = 2
 }
+
+module "github-repository" {
+  source                   = "github.com/den-vasyliev/tf-github-repository"
+  github_owner             = var.GITHUB_OWNER
+  github_token             = var.GITHUB_TOKEN
+  repository_name          = var.FLUX_GITHUB_REPO
+  public_key_openssh       = module.tls_private_key.public_key_openssh
+  public_key_openssh_title = "flux0"
+}
+
+module "flux_bootstrap" {
+  source            = "github.com/den-vasyliev/tf-fluxcd-flux-bootstrap?ref=gke_auth"
+  github_repository = "${var.GITHUB_OWNER}/${var.FLUX_GITHUB_REPO}"
+  private_key       = module.tls_private_key.private_key_pem
+  github_token      = var.GITHUB_TOKEN
+  #config_path       = "~/.kube/config"
+  #config_host       = module.gke_cluster.config_host
+  #config_token      = module.gke_cluster.config_token
+  #config_ca         = module.gke_cluster.config_ca
+}
+
+module "tls_private_key" {
+  source = "github.com/den-vasyliev/tf-hashicorp-tls-keys"
+}
--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,20 +1,24 @@
 variable "GOOGLE_PROJECT" {
   type        = string
+  default     = "nirev23"
   description = "GCP project name"
 }
 
 variable "GOOGLE_REGION" {
   type        = string
+  default     = "us-central1-c"
   description = "GCP region to use"
 }
 
 variable "GKE_MACHINE_TYPE" {
   type        = string
+  default     = "g1-small"
   description = "Machine type"
 }
 
 variable "GKE_NUM_NODES" {
   type        = number
+  default     = "1"
   description = "GKE nodes number"
 }
 
